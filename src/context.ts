db34b828d6dd6ef663fb90ac7ff905e793a66a0a
emarson.halcyondigital <elbancud@gmail.com>
2022-08-05 13:29:05 +0800

    chore: request header decode aut header

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client';
-
+import { Request } from 'express';
+import { decodeAuthHeader } from './utils';
 // TODO: Export the client
 export const prisma = new PrismaClient();
 //type;
 export interface Context {
   prisma: PrismaClient;
+  userId?: number;
 }
-export const context: Context = {
-  prisma,
+export const context = ({ req }: { req: Request }): Context => {
+  const token =
+    req && req.headers.authorization
+      ? decodeAuthHeader(req.headers.authorization)
+      : null;
+  return {
+    prisma,
+    userId: token?.userId,
+  };
 };
