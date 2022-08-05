486e3b5851e61a94251103fe17c4d6eb0155621c
emarson.halcyondigital <elbancud@gmail.com>
2022-08-05 11:53:49 +0800

    chore: auth login implemented, kapoi

--- src/graphql/Auth.ts
+++ src/graphql/Auth.ts
@@ -2,6 +2,7 @@ import * as bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
 import { extendType, nonNull, objectType, stringArg } from 'nexus';
 import { APP_SECRET_KEY } from '../utils';
+
 export const AuthPayload = objectType({
   name: 'AuthPayload',
   definition(t) {
@@ -15,6 +16,33 @@ export const AuthPayload = objectType({
 export const AuthMutation = extendType({
   type: 'Mutation',
   definition(t) {
+    t.nonNull.field('login', {
+      type: 'AuthPayload',
+      args: {
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      async resolve(parent, args, context, info) {
+        const { email } = args;
+        const user = await context.prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          throw new Error('No such user found');
+        }
+        const valid = await bcrypt.compare(args.password, user.password);
+        if (!valid) {
+          throw new Error('Invalid credentials');
+        }
+        const token = await jwt.sign({ userId: user.id }, APP_SECRET_KEY);
+        return {
+          user,
+          token,
+        };
+      },
+    });
     t.nonNull.field('signup', {
       type: 'AuthPayload',
       args: {
