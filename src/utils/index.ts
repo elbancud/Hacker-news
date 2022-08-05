421069b85a171fab6f1956a05c78a1d178d2f0ac
emarson.halcyondigital <elbancud@gmail.com>
2022-08-05 11:59:07 +0800

    chore: decode auth header bearer

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,12 @@
+import * as jwt from 'jsonwebtoken';
 export const APP_SECRET_KEY = 'bulagt0k$uMUk0k4n$';
+export interface AuthTokenPayload {
+  userId: number;
+}
+export function decodeAuthHeader(authHeader: string): AuthTokenPayload {
+  const token = authHeader.replace('Bearer', '');
+  if (!token) {
+    throw new Error('No token found');
+  }
+  return jwt.verify(token, APP_SECRET_KEY) as AuthTokenPayload;
+}
